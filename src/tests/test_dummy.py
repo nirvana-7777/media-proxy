08060baf76e56c6ebda7141b167aa66ee58ab045
Nirvana <Nirvana@nirvana.com>
2026-01-18 16:22:59 +0100

    initial

---
+++ src/tests/test_dummy.py
@@ -0,0 +1,14 @@
+"""Dummy test to make pytest pass until real tests are written"""
+
+
+def test_dummy():
+    """Temporary test to make pytest pass"""
+    assert True
+
+
+class TestDummyClass:
+    """Temporary test class"""
+
+    def test_class_method(self):
+        """Another temporary test"""
+        assert 1 + 1 == 2
